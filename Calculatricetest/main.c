55d4f964d2a8069dcb1c7821cacf9535694fe89c
Zbeulitox <44438010+Zbeulitox@users.noreply.github.com>
2018-10-25 10:15:38 +0200

    resourdre conflit

--- Calculatricetest/main.c
+++ Calculatricetest/main.c
@@ -8,8 +8,6 @@ int addition (int a,int b)
 
 int main ()
 {
-<<<<<<< HEAD
-    printf ("a+b=%d\n", addition (1,4));
     printf ("L'addition de a et de b vaut %d\n", addition (1,4));
     return 0;
 }
